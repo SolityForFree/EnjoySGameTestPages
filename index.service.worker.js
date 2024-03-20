6b67ba3923d17cd12c7bf93f9e5675f934b1a815
SolityForFree <strax950@gmail.com>
2024-03-20 23:57:05 +0100

    Added info about additional data.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1710363660|2100620339";
+const CACHE_VERSION = "1710975289|32345011282";
 const CACHE_PREFIX = "EnjoySGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
