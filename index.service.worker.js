8d7e562d6096083ed68074f3af672d39efde7898
Randomone. <strax950@gmail.com>
2024-03-27 05:29:09 -0700

    New version with login and so.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1711028825|41448150";
+const CACHE_VERSION = "1711542532|27923931";
 const CACHE_PREFIX = "EnjoySGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
